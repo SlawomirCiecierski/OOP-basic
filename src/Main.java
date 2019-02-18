2f753a8314742ce4585cee84ef7c9e7debe0f5d6
Slawomir Ciecierski <ciecierski@post.pl>
2019-02-18 12:10:20 +0100

    Big commit, 18.2.19 12:10

--- src/Main.java
+++ src/Main.java
@@ -1,25 +1,40 @@
 import model.Auto;
 
 public class Main {
-    public static void main(String[] args) {
-        //utworzenie obiektu klasy
-        Auto a1 = new Auto();
-        //odwołanie do pól klasowych
-        System.out.println("Brand "+a1.brand);
-        System.out.println("Model "+a1.model);
-        System.out.println("Production "+a1.year);
-        System.out.println("Color: "+a1.color);
-        System.out.println("Paliwo: "+a1.fuel_type);
-        System.out.println("Cena: "+a1.price);
-        a1.brand="BMW";
-        a1.model="X5";
-        a1.year=2010;
-        a1.color="Black";
-        a1.fuel_type="Diesel";
-        a1.segment='S';
-        a1.price=98_000;
-        System.out.println(a1);
-        //ALT + INS
+//    public static void main(String[] args) {
+//        //utworzenie obiektu klasy
+//        Auto a1 = new Auto();
+//        //odwołanie do pól klasowych
+//        System.out.println("Brand "+a1.brand);
+//        System.out.println("Model "+a1.model);
+//        System.out.println("Production "+a1.year);
+//        System.out.println("Color: "+a1.color);
+//        System.out.println("Paliwo: "+a1.fuel_type);
+//        System.out.println("Cena: "+a1.price);
+//        a1.brand="BMW";
+//        a1.model="X5";
+//        a1.year=2010;
+//        a1.color="Black";
+//        a1.fuel_type="Diesel";
+//        a1.segment='S';
+//        a1.price=98_000;
+//        System.out.println(a1);
+//        // drugi obiekt klasy
+//        Auto a2 = new Auto();
+//        System.out.println(a2);
+//        a2.setPrice(100000);
+//        System.out.println("a2 = " + a2);
+//        System.out.println("cena: "+a2.getPrice());
+//        System.out.println("cena brutto: "+a2.getPriceBrutto(23));
 
-    }
+    Auto a3 = new
+    Auto(           "VW",
+                    "Passat",
+                    2019,
+                    "silver",
+                    "diesel",
+                    150000,
+                    'D');
+
+}
 }
--- src/model/Auto.java
+++ src/model/Auto.java
@@ -7,7 +7,27 @@ public class Auto {
     public String color;
     public String fuel_type;
     public double price;
+    public char segment;
 
+    //konstruktor
+    public Auto(String brand,
+                String model,
+                int year,
+                String color,
+                String fuel_type,
+                double price,
+                char segment){
+        this.brand=brand;
+        this.model=model;
+        this.year=year;
+        this.color=color;
+        this.fuel_type=fuel_type;
+        this.price=price;
+        this.segment=segment;
+    }
+
+
+    //alt + ins
     @Override
     public String toString() {
         return "Auto{" +
@@ -19,7 +39,20 @@ public class Auto {
                 ", price=" + price +
                 ", segment=" + segment +
                 '}';
+
+
     }
 
-    public char segment;
+    public void setPrice(double newPrice) {
+        this.price = newPrice;
+    }
+
+    public double getPrice() {
+        return this.price;
+    }
+
+    public double getPriceBrutto(int tax) {
+        return this.price * (1 + (tax / 100.));
+        //return getPrice() * (1 + ((double)tax / 100));
+    }
 }
